743d3c31e22a8ddb3c0e487131902c2f5e31611c
JaimeRax <jaimecaal60@gmail.com>
2024-06-24 23:44:02 -0600

    :construction: (wip): add main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import "./style.css";
 // import "./topics/01-basic-types";
 // import "./topics/02-object-interface";
+// import "./topics/03-functions";
+// import "./topics/04-homework-types";
+// import "./topics/05-basic-destructuring";
+// import "./topics/06-function-destruring";
+// import "./topics/07-import-export";
+import "./topics/08-classes";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 hola mundo
